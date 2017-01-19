13dbfc127d3e9e15465cf6389d74db69a9d137d1
geovas <geovas333@gmail.com>
2017-01-19 19:01:26 +0300

    version v1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Arduino-Imperial-March #
+
+In this simple project arduino makes two things:
+* Sing an Imperial March (a part of it)
+* Blink led's in tact with music
+
+More info here: http://www.instructables.com/id/Darth-Vader-Papercraft-Toy/
---
+++ imperial/imperial.ino
@@ -0,0 +1,53 @@
+<<<<<<< HEAD
+#define Ab3 207.65
+#define C4  261.63
+#define E4  329.63
+#define F3  174.61
+#define F4  349.23
+#define LA3 220.00
+
+#define Q 500
+#define E Q/2
+#define S Q/4
+#define H Q*2
+
+#define VCC 3 // VCC pin (3.3v)
+#define GND 4 // GND pin (0.0v)
+#define SPK 1 // speaker pin
+#define RED 4 // led pin
+
+// ---------------------------------------------------------------------------------
+
+// Imperial March (partition)
+unsigned int note[] = {LA3,LA3,LA3,F3,C4, LA3,F3,C4,LA3, E4,E4,E4,F4,C4, Ab3,F3,C4,LA3};
+unsigned long duration[] = {Q,Q,Q,E+S,S, Q,E+S,S,H, Q,Q,Q,E+S,S, Q,E+S,S,H};
+unsigned int TOTAL = 18;
+
+// ---------------------------------------------------------------------------------
+
+void setup(void)
+{
+  pinMode(VCC,OUTPUT);
+  digitalWrite(VCC, HIGH);
+  
+  pinMode(GND,OUTPUT);
+  digitalWrite(GND, LOW);
+  
+  pinMode(SPK,OUTPUT);
+  digitalWrite(SPK, LOW);
+  
+  pinMode(RED,OUTPUT);
+  digitalWrite(RED, LOW);
+}
+
+void loop(void)
+{
+  static int i = 0;
+  tone(SPK, note[i], duration[i]);
+  delay(1+duration[i]);
+
+  i++;
+  if (i >= TOTAL) i = 0;
+  
+  analogWrite(RED, duration[i] / 4);
+}
